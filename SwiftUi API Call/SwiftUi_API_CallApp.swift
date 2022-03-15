9c915be5e7ba0e752d61864165e67282c85b2754
karis <kimani.eric@eclectics.io>
2022-03-15 17:05:44 +0300

    Initial Commit

---
+++ SwiftUi API Call/SwiftUi_API_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_API_CallApp.swift
+//  SwiftUi API Call
+//
+//  Created by Eclectics on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_API_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
